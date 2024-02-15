fa48edf44c6b5088a6a8e6cf2b7e33847e73bb52
Yordan Stoyanov <yordan.y.stoyanov@gmail.com>
2024-02-15 13:02:25 +0200

    edit factory method abstract example

---
+++ src/factorymethod/abstractcreator/RectangleFactory.java
@@ -0,0 +1,14 @@
+package factorymethod.abstractcreator;
+
+public class RectangleFactory implements ShapeFactory {
+    private final Shape rectangle;
+
+    public RectangleFactory(double width, double length) {
+        this.rectangle = new Rectangle(width, length);
+    }
+
+    @Override
+    public Shape getShape() {
+        return rectangle;
+    }
+}
